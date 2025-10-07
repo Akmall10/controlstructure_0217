8de5078a5f59a2793b02ce14d1097023d8a54ea1
Akmall10 <akmalprasety53@gmail.com>
2025-10-07 15:13:53 +0700

    firstcommit prak2

--- prak1.py
+++ prak1.py
@@ -2,4 +2,5 @@ percentage = float(input("Masukan persentase nilai siswa:"))
 
 if percentage >= 90:
     print("Excellent performance")
-   
+elif percentage >= 80:
+    print("Very Good performance")
